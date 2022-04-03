18b627f9c4ef9b2f48faef0bd78963823f3bd290
Gr1ngoire <80485151+Gr1ngoire@users.noreply.github.com>
2022-04-03 10:52:55 +0300

    Add files via upload

---
+++ LinkedListQueue.py
@@ -0,0 +1,17 @@
+from LinkedList import MyLinkedList
+
+class LinkedListQueue:
+
+    def __init__(self):
+        self.queue = MyLinkedList()
+
+    def put(self, data):
+        self.queue.append(data)
+
+    def remove(self):
+        self.queue.delete_start()
+
+    def element(self):
+
+        return self.queue.head.data
+
